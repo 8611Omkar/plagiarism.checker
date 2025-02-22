2b01fcbb14948a36c4d7c4090ef78af2c5cce103
Omkar gupta <99602051+8611Omkar@users.noreply.github.com>
2025-02-22 22:11:28 +0530

    Update settings.py

--- plagiarism_checker/settings.py
+++ plagiarism_checker/settings.py
@@ -81,6 +81,11 @@ DATABASES = {
     }
 }
 
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
